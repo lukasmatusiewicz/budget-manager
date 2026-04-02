713c047b024b8c5214526515e140cd41fd290e89
lukasmatusiewicz <matusiewiczlukas@gmail.com>
2026-04-02 14:04:14 +0200

    Update Reports.jsx

--- src/views/Reports/Reports.jsx
+++ src/views/Reports/Reports.jsx
@@ -1,4 +1,5 @@
 import CategoryStructure from '../../components/organisms/CategoryStructure/CategoryStructure.jsx';
+import DailyStats from '../../components/organisms/DailyStats/DailyStats.jsx';
 import './Reports.css';
 
 const Reports = () => {
@@ -6,6 +7,7 @@ const Reports = () => {
     <div className="view-container">
       <h2>Financial Reports</h2>
       <div className="reports-dashboard">
+        <DailyStats />
         <CategoryStructure type="expense" title="Expenses Structure" />
         <CategoryStructure type="income" title="Incomes Structure" />
       </div>
