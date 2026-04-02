cd958c874bb78cf75e1cf8c0b0f0259f0c7bc529
lukasmatusiewicz <matusiewiczlukas@gmail.com>
2026-04-02 14:19:51 +0200

    Update Settings.jsx

--- src/views/Settings/Settings.jsx
+++ src/views/Settings/Settings.jsx
@@ -1,4 +1,5 @@
 import ThemeSettings from '../../components/organisms/ThemeSettings/ThemeSettings.jsx';
+import TransactionPreferences from '../../components/organisms/TransactionPreferences/TransactionPreferences.jsx';
 import DangerZone from '../../components/organisms/DangerZone/DangerZone.jsx';
 
 const Settings = () => {
@@ -6,6 +7,7 @@ const Settings = () => {
     <div className="view-container">
       <h2>Settings</h2>
       <ThemeSettings />
+      <TransactionPreferences />
       <DangerZone />
     </div>
   );
