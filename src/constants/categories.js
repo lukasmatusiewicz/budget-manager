4dcd57e7a17e57b681bf6958cc8f6a546ea7afae
lukasmatusiewicz <matusiewiczlukas@gmail.com>
2026-04-02 12:53:05 +0200

    Create categories.js

---
+++ src/constants/categories.js
@@ -0,0 +1,17 @@
+export const EXPENSE_CATEGORIES = [
+  'Food',
+  'Transport',
+  'Housing',
+  'Entertainment',
+  'Health',
+  'Shopping',
+  'Other'
+];
+
+export const INCOME_CATEGORIES = [
+  'Salary',
+  'Freelance',
+  'Investment',
+  'Gift',
+  'Other'
+];
