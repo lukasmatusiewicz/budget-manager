af4691f1eed686d89b67da649de5f4a1188d47d8
lukasmatusiewicz <matusiewiczlukas@gmail.com>
2026-04-02 12:16:04 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import transactionReducer from './slices/transactionSlice.js';
 import authReducer from './slices/authSlice.js';
+import themeReducer from './slices/themeSlice.js';
 
 export const store = configureStore({
   reducer: {
     transactions: transactionReducer,
     auth: authReducer,
+    theme: themeReducer,
   },
 });
