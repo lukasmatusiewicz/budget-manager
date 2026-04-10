710968f4cecd8960abd998bd74bbab56e9f3439b
lukasmatusiewicz <matusiewiczlukas@gmail.com>
2026-04-10 16:18:59 +0200

    Update Reports.jsx

--- src/views/Reports/Reports.jsx
+++ src/views/Reports/Reports.jsx
@@ -2,6 +2,11 @@ import { useTranslation } from 'react-i18next';
 import CategoryStructure from '../../components/organisms/CategoryStructure/CategoryStructure.jsx';
 import DailyStats from '../../components/organisms/DailyStats/DailyStats.jsx';
 import QuickStatsTable from '../../components/organisms/QuickStatsTable/QuickStatsTable.jsx';
+import MonthlySpendingTrends from '../../components/organisms/MonthlySpendingTrends/MonthlySpendingTrends.jsx';
+import CategoryComparison from '../../components/organisms/CategoryComparison/CategoryComparison.jsx';
+import TopMerchants from '../../components/organisms/TopMerchants/TopMerchants.jsx';
+import IncomeVsExpense from '../../components/organisms/IncomeVsExpense/IncomeVsExpense.jsx';
+import SavingsRateChart from '../../components/organisms/SavingsRateChart/SavingsRateChart.jsx';
 import './Reports.css';
 
 const Reports = () => {
@@ -15,6 +20,11 @@ const Reports = () => {
         <CategoryStructure type="income" title={t('reports.incomes_structure')} />
         <QuickStatsTable />
         <DailyStats />
+        <IncomeVsExpense />
+        <MonthlySpendingTrends />
+        <CategoryComparison />
+        <TopMerchants />
+        <SavingsRateChart />
       </div>
     </div>
   );
