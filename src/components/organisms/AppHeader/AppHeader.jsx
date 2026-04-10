624bcc1c1eaa6d304da00716d4d7788eabacf690
lukasmatusiewicz <matusiewiczlukas@gmail.com>
2026-04-10 12:03:06 +0200

    add link in banner

--- src/components/organisms/AppHeader/AppHeader.jsx
+++ src/components/organisms/AppHeader/AppHeader.jsx
@@ -1,17 +1,20 @@
+import { Link } from 'react-router-dom';
 import Icon from '../../atoms/Icon/Icon.jsx';
 import './AppHeader.css';
 
 const AppHeader = ({ title }) => {
   return (
     <header className="app-header">
-      <div className="header-content">
-        <div className="logo-container">
-          <Icon name="wallet-icon" className="header-logo" />
+      <Link to="/" className="header-link">
+        <div className="header-content">
+          <div className="logo-container">
+            <Icon name="wallet-icon" className="header-logo" />
+          </div>
+          <div className="title-container">
+            <h1>{title}</h1>
+          </div>
         </div>
-        <div className="title-container">
-          <h1>{title}</h1>
-        </div>
-      </div>
+      </Link>
       <div className="header-decoration"></div>
     </header>
   );
